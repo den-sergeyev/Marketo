c15efa5f68c86d173885a09524cef4de45b5dd09
Artur Shybaila <Artur_Shybaila@epam.com>
2014-01-10 18:40:04 +0300

    Responding to CR

--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -13,13 +13,16 @@ describe Marketo do
 
     describe "merge_params!" do
       it "merges hash of params to existing config" do
-        test_hash = { access_key: "some_key", secret_key: "some_secret" }
+        test_hash = { access_key: "some_key",
+                      secret_key: "some_secret",
+                      wsdl_endpoint: "https://some.mktoapi.com/soap/mktows/2_2"
+                    }
         config_hash = Marketo::Config.default
         config_hash.merge_params!(test_hash)
         config_hash.wsdl_document.should == "https://app.marketo.com/soap/mktows/2_2?WSDL"
         config_hash.access_key.should == "some_key"
         config_hash.secret_key.should == "some_secret"
-        config_hash.wsdl_endpoint.should be_nil
+        config_hash.wsdl_endpoint.should == "https://some.mktoapi.com/soap/mktows/2_2"
       end
     end
   end
