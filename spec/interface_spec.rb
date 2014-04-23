b140fd989d4d28dcc5ea6700c8dbec9f3e05f766
Den Sergeyev <dzianis_siarheyeu@epam.com>
2014-04-23 22:28:06 +0300

    Remove useless constants from test.

--- spec/interface_spec.rb
+++ spec/interface_spec.rb
@@ -1,13 +1,7 @@
 require_relative 'spec_helper'
-require "marketo"
 
 describe Marketo do
   describe Marketo::Interface do
-    IDNUM = 1000074
-    EMAIL = "john@backupify.com"
-    COOKIE = "id:572-ZRG-001&token:_mch-localhost-1306412206125-92040"
-    USER = { :email => "john@backupify.com", :first_name => "john", :last_name => "kelly" }
-
     describe 'to_hash' do
       it 'should update timestamp on every call to avoid request expiration' do
         client = Marketo::Client.new_marketo_client
